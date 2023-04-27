4bb37863ea2533279056f84b6b23002dad018e75
liaoyi <2417276459@qq.com>
2023-04-27 12:21:32 +0800

    Feat：change lang

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Redirect } from '@docusaurus/router'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-	return <Redirect to="/docs/overall/create" />
+	let [lang, setLang] = useState('en')
+	useEffect(() => {
+		setLang(document.documentElement.lang)
+	})
+	return <Redirect to={lang == 'zh-Hans' ? '/zh-Hans/docs/overall/create' : '/docs/overall/create'} />
 }
